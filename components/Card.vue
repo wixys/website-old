50e0df77e6a158f5d8389fc82dcc1c1575e49ab5
Willfrans <willi@wixys.com>
2019-12-30 00:31:22 +0100

    Added Component Card

--- components/Card.vue
+++ components/Card.vue
@@ -3,19 +3,23 @@
     <b-container>
       <b-row class="mt-2">
         <b-card-group deck>
-          <b-card
+          <b-col
             v-for="(item, index) in items"
             :key="index"
+            :data-index="index"
             class="col-lg-4 col-md-4 mb-3"
-            :header="item.title"
-            header-tag="header"
-            footer="Card Footer"
-            footer-tag="footer"
-            :title="item.title"
           >
-            <b-card-text v-html="item.description"></b-card-text>
-            <b-button href="#" variant="primary">Go somewhere</b-button>
-          </b-card>
+            <b-card
+              :header="item.title"
+              header-tag="header"
+              footer="Card Footer"
+              footer-tag="footer"
+              :title="item.title"
+            >
+              <b-card-text v-html="item.description"></b-card-text>
+              <b-button href="#" variant="primary">Go somewhere</b-button>
+            </b-card>
+          </b-col>
         </b-card-group>
       </b-row>
     </b-container>
